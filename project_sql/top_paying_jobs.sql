b302b583b95d02f3fb2c2947e221efba740d347f
Mohamed Amine Moatadid <mohamedaminemoatadid4@gmail.com>
2026-08-20 18:13:28 +0800

    upload sql files

--- project_sql/top_paying_jobs.sql
+++ project_sql/top_paying_jobs.sql
@@ -1,1 +1,22 @@
--- delete this later
+/*
+Question: what are the top paying data analyst jobs that are remote (work from home) and have a salary listed?
+*/
+
+SELECT
+    job_id,
+    job_title,
+    job_location,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name as company_name
+FROM 
+    job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE 
+    salary_year_avg IS NOT NULL AND 
+    job_title_short = 'Data Analyst' AND 
+    job_location = 'Anywhere'
+ORDER BY 
+    salary_year_avg DESC
+LIMIT 10;
